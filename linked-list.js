d71cc07f9ce26f299e53c6926f13712aaf7647d0
Forkentiney <jseverud@proton.me>
2026-06-11 17:34:16 -0500

    Append is working. Think I can figure out the rest.

--- linked-list.js
+++ linked-list.js
@@ -1,8 +1,37 @@
-function linkedList(value) {
-	const append = (value) => {};
+const createLinkedList = () => {
+	let head = null;
+	let size = 0;
+	
+	const append = (value) => {
+		const newNode = createNode(value);
+		if (!head) {
+			head = newNode;
+		} else {
+			let current = head;
+			while (current.next) {
+				current = current.next;
+			}
+			current.next = newNode;
+		}
+		size++;
+	};
+
+	const displayHead = () => head;
+	const displaySize = () => size;
+
+	return { append, displayHead, displaySize };
 };
 
-function node() => {
-	const value = null;
-	const nextNode = null;
-}();
+const createNode = (value = null, next = null) => {
+	return { value, next };
+};
+
+const list = createLinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("tiger");
+
+console.log(list.displaySize());
+console.log(list.displayHead());
+console.log(list.head.next);
