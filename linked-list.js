ac026e8ed55586c1908662a058d86727350cc6da
Forkentiney <jseverud@proton.me>
2026-06-15 15:15:50 -0500

    contains() is working

--- linked-list.js
+++ linked-list.js
@@ -45,11 +45,23 @@ const createLinkedList = () => {
 		return `The value ${oldHead.value} has been removed. The new head is ${head.value}.`;
 	};
 
+	const contains = (value) => {
+		let current = head;
+		while (current && current.value !== value) {
+			current = current.next;
+		};
+		if (current === null) {
+			return false;
+		} else {
+			return true;
+		};
+	};
+
 	const displayHead = () => head;
 	const displayTail = () => tail;
 	const displaySize = () => size;
 
-	return { append, prepend, displayHead, displayTail, at, pop, displaySize };
+	return { append, prepend, displayHead, displayTail, at, pop, contains, displaySize };
 };
 
 const createNode = (value = null, next = null) => {
@@ -63,4 +75,8 @@ list.append("cat");
 list.append("tiger");
 list.prepend("salamander");
 
-console.log(list.pop())
+console.log(list.contains("tiger"));
+console.log(list.contains("spider"));
+console.log(list.contains("salamander"));
+list.append("spider");
+console.log(list.contains("spider"));
